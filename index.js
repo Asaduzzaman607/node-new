5943fbca5f22e7e4b84c6507cc5e8db3bf054305
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-09-26 17:59:39 +0600

    node mongo updated

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("I know how to open node");
+app.use(cors());
+app.use(bodyParser.json());
+
+const users = ["asad", "moin", "sohana", "shushmita", "sajeeb", "sabana"];
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const name = users[id];
+  res.send({ id, name });
+});
+
+//post
+app.post("/addUser", (req, res) => {
+  console.log("data received", req.body);
 });
 
 app.listen(3000, () => console.log("listening to port 3000"));
